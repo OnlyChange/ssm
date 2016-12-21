e164ffe943bb0bfe999125ab868a0d632f75b209
lime <365262335@qq.com>
2016-12-21 15:14:07 +0800

    modify

--- ssm/src/test/java/com/ssm/fork/Main.java
+++ ssm/src/test/java/com/ssm/fork/Main.java
@@ -5,42 +5,38 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Main
- * {@link http://blog.csdn.net/coslay/article/details/45542853}
+ * Main {@link http://blog.csdn.net/coslay/article/details/45542853}
+ * 
  * @author limn
- * @since  2016年12月20日 下午5:19:32
+ * @since 2016年12月20日 下午5:19:32
  */
 public class Main {
 	public static void main(String[] args) {
-		ProductListGenerator generator=new ProductListGenerator();
-		List<Product> products=generator.generate(10000);
-		Task task=new Task(products, 0, products.size(), 0.20);
-		ForkJoinPool pool=new ForkJoinPool();
+		ProductListGenerator generator = new ProductListGenerator();
+		List<Product> products = generator.generate(10000);
+		Task task = new Task(products, 0, products.size(), 0.20);
+		ForkJoinPool pool = new ForkJoinPool();
 		pool.execute(task);
 		do {
-		    System.out.printf("Main: Thread Count: %d\n",pool.
-		    getActiveThreadCount());
-		    System.out.printf("Main: Thread Steal: %d\n",pool.
-		    getStealCount());
-		    System.out.printf("Main: Parallelism: %d\n",pool.
-		    getParallelism());
+			System.out.printf("Main: Thread Count: %d\n", pool.getActiveThreadCount());
+			System.out.printf("Main: Thread Steal: %d\n", pool.getStealCount());
+			System.out.printf("Main: Parallelism: %d\n", pool.getParallelism());
 
-		try {
-		    TimeUnit.MILLISECONDS.sleep(5);
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
+			try {
+				TimeUnit.MILLISECONDS.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		} while (!task.isDone());
 		pool.shutdown();
-		if (task.isCompletedNormally()){
-		    System.out.printf("Main: The process has completed normally.\n");
+		if (task.isCompletedNormally()) {
+			System.out.printf("Main: The process has completed normally.\n");
 		}
-		for (int i=0; i<products.size(); i++){
-		    Product product=products.get(i);
-		    if (product.getPrice()!=12) {
-		        System.out.printf("Product %s: %f\n",product.
-		        getName(),product.getPrice());
-		    }
+		for (int i = 0; i < products.size(); i++) {
+			Product product = products.get(i);
+			if (product.getPrice() != 12) {
+				System.out.printf("Product %s: %f\n", product.getName(), product.getPrice());
+			}
 		}
 		System.out.println("Main: End of the program.\n");
 	}
---
+++ ssm/src/test/java/com/ssm/tests/TestTemp.java
@@ -0,0 +1,13 @@
+package com.ssm.tests;
+/**
+ * TestTemp
+ * @author limn
+ * @since  2016年12月21日 上午11:38:18
+ */
+public class TestTemp {
+	
+	public static void main(String[] args) {
+		System.out.println(Runtime.getRuntime().availableProcessors());
+		System.out.println(0x7fff);
+	}
+}
